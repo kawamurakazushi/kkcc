7db37ef1d94f8bd47daf6c8ff8edfdb1ae75d0c1
Kazushi Kawamura <wakaka2277@gmail.com>
2020-04-23 23:46:43 +0900

    feat: improve error messsage

--- kkcc.c
+++ kkcc.c
@@ -24,11 +24,19 @@ struct Token
 
 Token *token;
 
-// For Error
-void error(char *fmt, ...)
+// Input program
+char *user_input;
+
+// Reports an error location and exit
+void error_at(char *loc, char *fmt, ...)
 {
   va_list ap;
   va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, "");
+  fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
@@ -47,14 +55,14 @@ bool consume(char op)
 void expect(char op)
 {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("'%c'ではありません", op);
+    error_at(token->str, "'%c'ではありません", op);
   token = token->next;
 }
 
 int expect_number()
 {
   if (token->kind != TK_NUM)
-    error("Not a number");
+    error_at(token->str, "Not a number");
 
   int val = token->val;
   token = token->next;
@@ -76,6 +84,7 @@ Token *new_token(TokenKind kind, Token *cur, char *str)
   return tok;
 }
 
+// Tokenize `user_input` and returns new tokens.
 Token *tokenize(char *p)
 {
   Token head;
@@ -103,7 +112,7 @@ Token *tokenize(char *p)
       continue;
     }
 
-    error("tokenize failed");
+    error_at(p, "Tokenize failed");
   }
 
   new_token(TK_EOF, cur, p);
@@ -119,6 +128,7 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  user_input = argv[1];
   token = tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
